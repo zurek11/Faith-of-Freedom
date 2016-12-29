1372de760cf1e0ccea41554334fad7a8cbbd6242
zurek11 <adamzurek14@gmail.com>
2016-12-29 15:50:13 +0100

    0.0.1 Pre-alpha

--- desktop/src/sk/zurek/fof/desktop/DesktopLauncher.java
+++ desktop/src/sk/zurek/fof/desktop/DesktopLauncher.java
@@ -2,11 +2,12 @@ package sk.zurek.fof.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import sk.zurek.fof.Faith-of-Freedom;
+import sk.zurek.FOF.FOF;
+
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Faith-of-Freedom(), config);
+		new LwjglApplication(new FOF(), config);
 	}
 }
